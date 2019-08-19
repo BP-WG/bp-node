73ae835f223f8ad5b93aef6daf2bbd7e60901722
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2019-08-19 23:54:30 +0200

    Basic app skeleton and block parsing

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bitcoin protocol Swiss army knife - like `tcpdump`, but for LNP/BP stack
+
+WIP
+
+```sh
+$ cargo run --package bpdump --features yaml --bin bpdump block sample_data/blk00000.dat
+```
---
+++ src/main.rs
@@ -0,0 +1,75 @@
+#[macro_use]
+extern crate clap;
+extern crate bitcoin;
+
+use std::fs::File;
+use std::io;
+use clap::{App, ArgMatches};
+use std::io::BufRead;
+use bitcoin::network::stream_reader::StreamReader;
+use bitcoin::{Block, BitcoinHash};
+
+
+#[cfg(feature = "yaml")]
+fn main() -> std::io::Result<()> {
+    eprintln!("\nBitcoin protocol dumping tool\n");
+
+    let yaml = load_yaml!("../cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    match matches.value_of("COMMAND").unwrap() {
+        "block" => parse_block(matches),
+        _ => unreachable!(),
+    }
+}
+
+#[cfg(not(feature = "yaml"))]
+fn main() {
+    // As stated above, if clap is not compiled with the YAML feature, it is disabled.
+    eprintln!("YAML feature is disabled.");
+    eprintln!("Pass --features yaml to cargo when trying this example.");
+}
+
+fn parse_block(matches: ArgMatches) -> std::io::Result<()> {
+    let filename = matches.value_of("INPUT");
+    eprintln!("Parsing block file {}", filename.unwrap_or("STDIN"));
+
+    let mut stdin;
+    let buf_reader: Box<BufRead> = match filename {
+        Some(name) => Box::new(io::BufReader::new(File::open(name)?)),
+        None => {
+            stdin = io::stdin();
+            Box::new(stdin.lock())
+        },
+    };
+    let mut stream_reader = StreamReader::new(Box::new(buf_reader), None);
+
+    let mut blkno = 0;
+    loop {
+        // Reading magick number
+        match stream_reader.read_next::<u32>() {
+            Ok(0xD9B4BEF9) => (),
+            _ => {
+                eprintln!("No magick number found");
+                break;
+            }
+        }
+        // Skipping block length
+        eprintln!("Magick number ok");
+        let _ = stream_reader.read_next::<u32>();
+        // Reading block
+        match stream_reader.read_next::<Block>() {
+            Err(err) => {
+                eprintln!("{}", err);
+                break;
+            },
+            Ok(block) => {
+                eprintln!("* read block no {}, id {}", blkno, block.bitcoin_hash());
+                println!("{:#?}", block.header);
+                println!("{:#?}", block.txdata[0]);
+                blkno += 1;
+            },
+        }
+    }
+    Ok(())
+}
