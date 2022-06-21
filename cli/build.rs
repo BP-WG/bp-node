ceef14d103db2d86059a8ba1eb3e46d30c06a868
Dr Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-06-21 10:12:43 +0200

    Fix build scripts

--- cli/build.rs
+++ cli/build.rs
@@ -1,10 +1,9 @@
-// Storage daemon (stored): microservice frontend for different storage backends
-// used in LNP/BP nodes.
+// BP Node: bitcoin blockchain indexing and notification service
 //
-// Written in 2022 by
+// Written in 2020-2022 by
 //     Dr. Maxim Orlovsky <orlovsky@lnp-bp.org>
 //
-// Copyright (C) 2022 by LNP/BP Standards Association, Switzerland.
+// Copyright (C) 2020-2022 by LNP/BP Standards Association, Switzerland.
 //
 // You should have received a copy of the MIT License along with this software.
 // If not, see <https://opensource.org/licenses/MIT>.
@@ -20,15 +19,15 @@ use clap::IntoApp;
 use clap_complete::generate_to;
 use clap_complete::shells::*;
 
-pub mod bpd {
+pub mod cli {
     include!("src/opts.rs");
 }
 
 fn main() -> Result<(), configure_me_codegen::Error> {
-    let outdir = "./shell";
+    let outdir = "../shell";
 
     fs::create_dir_all(outdir).expect("failed to create shell dir");
-    for app in [bpd::Opts::command()].iter_mut() {
+    for app in [cli::Opts::command()].iter_mut() {
         let name = app.get_name().to_string();
         generate_to(Bash, app, &name, &outdir)?;
         generate_to(PowerShell, app, &name, &outdir)?;
