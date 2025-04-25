ba6aa927b305bba091900feda081c3b0643e5486
Dr Maxim Orlovsky <orlovsky@lnp-bp.org>
2025-04-25 12:53:14 +0200

    chore: update license plates

--- rpc/src/connectivity.rs
+++ rpc/src/connectivity.rs
@@ -5,24 +5,22 @@ use cyphernet::addr::{InetHost, NetAddr};
 //
 // SPDX-License-Identifier: Apache-2.0
 //
-// Written in 2020-2025 by
+// Designed & written in 2020-2025 by
 //     Dr Maxim Orlovsky <orlovsky@lnp-bp.org>
 //
 // Copyright (C) 2020-2024 LNP/BP Standards Association. All rights reserved.
 // Copyright (C) 2025 LNP/BP Labs, InDCS, Switzerland. All rights reserved.
 // Copyright (C) 2020-2025 Dr Maxim Orlovsky. All rights reserved.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//        http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions and limitations under
+// the License.
 
 pub type RemoteAddr = NetAddr<InetHost>;
 // For now, we use a very simple form of session: plain TCP stream
--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -2,24 +2,22 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 //
-// Written in 2020-2025 by
+// Designed & written in 2020-2025 by
 //     Dr Maxim Orlovsky <orlovsky@lnp-bp.org>
 //
 // Copyright (C) 2020-2024 LNP/BP Standards Association. All rights reserved.
 // Copyright (C) 2025 LNP/BP Labs, InDCS, Switzerland. All rights reserved.
 // Copyright (C) 2020-2025 Dr Maxim Orlovsky. All rights reserved.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//        http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions and limitations under
+// the License.
 
 #[macro_use]
 extern crate amplify;
@@ -44,4 +42,3 @@ pub use request::Request;
 pub use response::Response;
 
 pub const BP_RPC_LIB: &str = "BPRPC";
-pub const BP_NODE_RPC_ENDPOINT: &str = "0.0.0.0:61961";
